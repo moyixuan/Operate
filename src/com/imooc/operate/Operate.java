e20b01f502f79afc072efaa31238d9a6e2cd80cf
莫以轩 <moyixuan136@gmail.com>
2020-12-11 18:19:06 +0800

    Initial commit

---
+++ src/com/imooc/operate/Operate.java
@@ -0,0 +1,12 @@
+package com.imooc.operate;
+
+public class Operate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//
+		//
+		System.out.println();
+	}
+
+}

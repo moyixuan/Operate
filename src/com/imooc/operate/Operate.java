e0818d874d5adb928f624ea33b66cf0920df50bc
莫以轩 <moyixuan136@gmail.com>
2020-12-12 18:22:15 +0800

    Java加减乘除
    Java加减乘除练习提交

--- src/com/imooc/operate/Operate.java
+++ src/com/imooc/operate/Operate.java
@@ -8,8 +8,29 @@ public class Operate {
 		int num1 = 10, num2 = 5;
 		result = num1 + num2;
 		
-		System.out.println("Java加法result=" + result);
-		//测试git
+		System.out.println("Java加法："+num1+"+"+num2+"=" + result);
+		//字符串链接  105
+		System.out.println(""+num1+num2);
+		
+		//减法
+		result = num1 - num2;
+		System.out.println("Java减法："+num1 +"-"+num2+"="+result);
+		
+		//乘法
+		result = num1 * num2;
+		System.out.println("Java乘法：" + num1+"*" + num2+"="+result);
+		
+		//除法
+		result = num1 / num2;
+		//分子分母都是整型时，结果为整除后的结果
+		System.out.println("Java除法：" + num1+"/" + num2+"="+result);
+		//分子分母都是小数时，结果为小数后的结果
+		System.out.println("13.0/5=" + 13.0/5);
+		
+		//求余数
+		result = 13 % num2;
+		System.out.println("13%"+num2+"="+result);
+		System.out.println("13.5%5="+(13.5%5));
 	}
 
 }
